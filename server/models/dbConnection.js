8884ffde18faa61f55282c261d0f6963a25dc65f
Abialbon Paul <dr.abialbon@gmail.com>
2017-10-31 15:20:38 -0400

    Fixed mongoose Promise

--- server/models/dbConnection.js
+++ server/models/dbConnection.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 module.exports.connect = (db_URL) => {
   mongoose.connect(db_URL, { useMongoClient: true });
 
+  mongoose.Promise = global.Promise;
+
   mongoose.connection
     .once('open', () => console.log('Connection to the database is established'))
     .on('error', () => process.exit(1))
